b2dc6cc8b668b05455aef127d697f87790a2c762
张德志 <zhangdezhi@zhangdezhideMacBook-Air.local>
2018-11-10 22:59:11 +0800

    导航条

--- src/router/Navigation.js
+++ src/router/Navigation.js
@@ -1,13 +1,61 @@
 import React,{ Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
+import { Menu, Icon, Button } from 'antd';
 
 
 class Navigation extends Component{
-    render(){
-         return (<div>
+    constructor(props){
+        super(props);
+        this.state = {
+            collapsed: false,
 
-         </div>)
+        }
+       
+    }
+    render(){
+         return (
+            <div className="navigation-wrapper">
+            <Menu
+                
+                defaultSelectedKeys={['1']}
+                theme="light"
+                mode='inline'
+                defaultOpenKeys={['1']}
+                inlineCollapsed={this.state.collapsed}
+
+            >
+                <Menu.Item key="1" className="Navigation-menu">
+                    <Icon type="mail" />
+                    <span>企业邮箱</span>
+                </Menu.Item>
+                <Menu.Item key="2" className="Navigation-menu">
+                    <Icon type="desktop" />
+                    <span>监测预警</span>
+                </Menu.Item>
+                <Menu.Item key="3" className="Navigation-menu">
+                    <Icon type="inbox" />
+                    <span>日常监管</span>
+                </Menu.Item>
+                <Menu.Item className="Navigation-menu">
+                    <Icon type="bank" />
+                    <span>应急救援</span>
+                </Menu.Item>
+                <Menu.Item className="Navigation-menu">
+                    <Icon type="trademark" />
+                    <span>安全知识库</span>
+                </Menu.Item>
+                <Menu.Item className="Navigation-menu">
+                    <Icon type="border" />
+                    <span>实景监控</span>
+                </Menu.Item>
+                <Menu.Item className="Navigation-menu">
+                    <Icon type="poweroff" />
+                    <span>办公辅助</span>
+                </Menu.Item>
+            </Menu>
+            </div>
+         )
     }
 }
 
