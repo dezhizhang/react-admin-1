2bbfb9eb55801a9b2e5abef33cdc78eca512c820
张德志 <zhangdezhi@zhangdezhideMacBook-Air.local>
2018-11-12 22:48:30 +0800

    直播

--- src/components/contentLive/ContentLive.js
+++ src/components/contentLive/ContentLive.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from  './ContentLive.css';
-
+import time1 from '../../common/imgs/time1.png'
 class ContentLive extends React.Component{
     render(){
         return (
@@ -9,11 +9,64 @@ class ContentLive extends React.Component{
               <div className={style.liveWrapper}>
                 <div className={style.liveTabbar}></div>
                 <div className={style.liveFlex}>
-                   <div className={`${style.liveItem} ${style.liveitemOne}`}></div>
-                   <div className={style.liveItem}></div>
-                   <div className={style.liveItem}></div>
-                   <div className={style.liveItem}></div>
-                   <div className={style.liveItem}></div>
+                   <div className={`${style.liveItem} ${style.liveitemOne}`}>
+                      <div className={`${style.liveList} ${style.liveTop}`}>
+                            <img src={time1} className={style.liveImage}/>
+                            <p className={`${style.font} ${style.center}`}>这段时光能</p>
+                            <span className={`${style.fontDesc} ${style.center}`}>这段时光能</span>
+                      </div>
+                      <div className={style.liveList}>
+                            <img src={time1} className={style.liveImage}/>
+                            <p className={`${style.font} ${style.center}`}>这段时光能</p>
+                            <span className={`${style.fontDesc} ${style.center}`}>这段时光能</span>
+                      </div>
+                   </div>
+                   <div className={style.liveItem}>
+                     <div className={`${style.liveList} ${style.liveTop}`}>
+                        <img src={time1} className={style.liveImage}/>
+                        <span>这段时光能一直持续就好了❤</span>
+                     </div>
+                     <div className={style.liveList}>
+                            <img src={time1} className={style.liveImage}/>
+                            <p className={`${style.font} ${style.center}`}>这段时光能</p>
+                            <span className={`${style.fontDesc} ${style.center}`}>这段时光能</span>
+                       </div>
+                   </div>
+                   <div className={style.liveItem}>
+                        <div className={`${style.liveList} ${style.liveTop}`}>
+                            <img src={time1} className={style.liveImage}/>
+                                <span>这段时光能一直持续就好了❤</span>
+                            </div>
+                        <div className={style.liveList}>
+                            <img src={time1} className={style.liveImage}/>
+                            <p className={`${style.font} ${style.center}`}>这段时光能</p>
+                            <span className={`${style.fontDesc} ${style.center}`}>这段时光能</span>
+                        </div>
+                   </div>
+                   <div className={style.liveItem}>
+                      <div className={`${style.liveList} ${style.liveTop}`}>
+                            <img src={time1} className={style.liveImage}/>
+                            <p className={`${style.font} ${style.center}`}>这段时光能</p>
+                            <span className={`${style.fontDesc} ${style.center}`}>这段时光能</span>
+                      </div>
+                      <div className={style.liveList}>
+                            <img src={time1} className={style.liveImage}/>
+                            <p className={`${style.font} ${style.center}`}>这段时光能</p>
+                            <span className={`${style.fontDesc} ${style.center}`}>这段时光能</span>
+                      </div>
+                   </div>
+                   <div className={style.liveItem}>
+                      <div className={`${style.liveList} ${style.liveTop}`}>
+                            <img src={time1} className={style.liveImage}/>
+                            <p className={`${style.font} ${style.center}`}>这段时光能</p>
+                            <span className={`${style.fontDesc} ${style.center}`}>这段时光能</span>
+                      </div>
+                      <div className={style.liveList}>
+                            <img src={time1} className={style.liveImage}/>
+                            <p className={`${style.font} ${style.center}`}>这段时光能</p>
+                            <span className={`${style.fontDesc} ${style.center}`}>这段时光能</span>
+                      </div>
+                   </div>
                 </div>
               </div>
            </div>
